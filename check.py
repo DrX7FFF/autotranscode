7bae69e1faadf5c1e617fcd927de770ad65bec89
DrX7FFF <dubourg.v@gmail.com>
2025-02-08 11:09:37 +0100

    save

--- check.py
+++ check.py
@@ -3,9 +3,9 @@ import sys
 import subprocess
 import json
 import datetime
+import csv
 
-separator = "|"
-modetest = True
+modetest = False
 
 if modetest:
     moviespath = "./examples"
@@ -14,14 +14,110 @@ else:
 # moviespath = "/home/moi/mediaHD1/Servarr/ToDo"
 ticketpath = "/home/moi/mediaHD1/Servarr/TranscodingTickets"
 
+def export_to_csv(data, filename="output.csv"):
+    """ Exporte une structure JSON dynamique en CSV tout en respectant l'ordre des clés. """
 
-keys_to_keep_in_stream = ["filename", "statut", "toencode", "toclean", "size", "resolution", "video_codec", "bitrate", "profile", "audio_codec", "sub", "comment", "modif"]
-keys_to_keep = {"video": ["avg_frame_rate", "codec_name", "profile", "codec_long_name"]}
-def filtrer_json(data):
-    tokeep = ["index", "codec_type"] 
-    if data["codec_type"] in keys_to_keep:
-        tokeep += keys_to_keep[data["codec_type"]]
-    return {k: v for k, v in data.items() if k in tokeep}
+    # Collecter toutes les clés sans modifier l'ordre
+    first_level_keys = []
+    stream_keys = []
+
+    for item in data:
+        for key in item.keys():
+            if key not in first_level_keys and key != "streams":
+                first_level_keys.append(key)
+        for stream in item.get("streams", []):
+            for key in stream.keys():
+                if key not in stream_keys:
+                    stream_keys.append(key)
+
+    all_keys = first_level_keys + stream_keys
+
+    with open(filename, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=all_keys, extrasaction="ignore")
+        writer.writeheader()
+
+        for item in data:
+            # Transformer les streams en un format aplati
+            for stream in item.get("streams", [{}]):
+                row = {key: item.get(key, "") for key in first_level_keys}
+                row.update({key: stream.get(key, "") for key in stream_keys})
+                writer.writerow(row)
+
+to_keep = {
+    "common":{"index":"index", "remove":"", "type":"codec_type", "codec":"codec_name"}, 
+    "video": {  "resolution":"width", "frame_rate": "avg_frame_rate", "profile":"profile"}, 
+    "audio": {   "language":["tags","language"], "title":["tags","title"], "channel":"channel_layout"}, 
+    "subtitle": {"language":["tags","language"], "title":["tags","title"]}
+    }
+
+# def filtrer_json(data):
+#     tokeep = ["index", "codec_type", "codec_name", "codec_long_name"] 
+#     if data["codec_type"] in to_keep.keys():
+#         tokeep += to_keep[data["codec_type"]]
+#     res = {k: v for k, v in data.items() if k in tokeep}
+#     return res
+
+    ## Traitement vidéo
+    # 4320p (8K) : 7 680 x 4 320.
+    # 2160p (4K) : 3 840 x 2 160.
+    # 1440p (2K) : 2 560 x 1 440.
+    # 1080p (HD) : 1 920 x 1 080.
+    # 720p (HD) : 1 280 x 7 20.
+    # 480p (SD) : 854 x 480.
+    # 360p (SD) : 640 x 360.
+    # 240p (SD) : 426 x 240.
+
+resolution_map = {
+    range(1900, 1940): '1080p',
+    range(1260, 1300): '720p',
+    range(834, 874): '480p',
+    range(620, 660): '360p',
+    range(406, 446): '240p'
+}
+
+def stream_treatment(stream):
+    match stream["type"]:
+        case "video":
+            if stream["frame_rate"] == "0/0":
+                stream["remove"] = True
+            else:
+                for r, res in resolution_map.items():
+                    if stream["resolution"]  in r:
+                        stream["resolution"] = res
+        case "audio":
+            if not stream["language"] in ["fre", "fra","", None]:
+                stream["remove"] = True
+        case "subtitle":
+            if stream["language"] != "fre" and stream["language"] != "fra" and stream["language"] != "":
+                stream["remove"] = True
+        case default:
+            stream["remove"] = True
+    return stream
+
+def get_value(data, path):
+    """ Récupère une valeur dans un dictionnaire avec un chemin (liste de clés). """
+    if isinstance(path, list):
+        for key in path:
+            data = data.get(key, {})
+        return data if data else None
+    return data.get(path)
+
+def filter_stream(stream):
+    """ Filtre un stream selon les clés définies dans to_keep. """
+    result = {}
+    
+    # Ajouter les valeurs communes
+    for new_key, old_key in to_keep["common"].items():
+        result[new_key] = get_value(stream, old_key)
+    
+    # Ajouter les valeurs spécifiques au type de stream
+    stream_type = result["type"]
+    if stream_type in to_keep:
+        for new_key, old_key in to_keep[stream_type].items():
+            result[new_key] = get_value(stream, old_key)
+    
+    return stream_treatment(result)
+
 
 def get_media_info(filepath):
     """Utilise ffprobe pour récupérer les infos du fichier."""
@@ -40,106 +136,60 @@ def get_media_info(filepath):
 def analyse_media(filename, filepath):
     filestats = os.stat(filepath)
 
-    res = {"filename": filename, "statut": "", "toencode":"No", "toclean":"No", "size": int(filestats.st_size/(1024*1024))/1000,  "resolution": "", "video_codec": "", "bitrate": "", "profile": "", "audio_codec":"", "sub":"", "comment":"", "modif": datetime.datetime.fromtimestamp(filestats.st_mtime).strftime("%d/%m/%Y %H:%M")}
+    res = {"filename": filename, "comment":"", "size": int(filestats.st_size/(1024*1024))/1000, "bitrate": "", "modif": datetime.datetime.fromtimestamp(filestats.st_mtime).strftime("%d/%m/%Y %H:%M"), "streams": []}
 
-    temp = []
     media_info = get_media_info(filepath)
-    for stream in media_info.get("streams", []):
-        toto = filtrer_json(stream)
-        temp.append(toto)
-    
     if not media_info:
-        res["statut"] = "Err"
         res["comment"] = "Erreur lors de l'analyse du fichier"
         return res
 
-    video_streams = [s for s in media_info.get("streams", []) if s.get("codec_type") == "video"]
+    streams = media_info.get("streams", [])
+    res["streams"] = [filter_stream(stream) for stream in streams]
+    
+
+    video_streams = [s for s in res["streams"] if s.get("type") == "video"]
     if not video_streams:
-        res["statut"] = "Err"
         res["comment"] = "Pas de flux vidéo"
         return res
     if len(video_streams) > 1:
-        res["statut"] = "Err"
         res["comment"] = "Plusieurs flux vidéo"
         return res
 
-    ## Traitement vidéo
-    # 4320p (8K) : 7 680 x 4 320.
-    # 2160p (4K) : 3 840 x 2 160.
-    # 1440p (2K) : 2 560 x 1 440.
-    # 1080p (HD) : 1 920 x 1 080.
-    # 720p (HD) : 1 280 x 7 20.
-    # 480p (SD) : 854 x 480.
-    # 360p (SD) : 640 x 360.
-    # 240p (SD) : 426 x 240.
-
-    width =video_streams[0].get('width', 0)
-    match width:
-        case 1920:
-            res["resolution"] = "1080p"
-        case 1280:
-            res["resolution"] = "720p"
-        case 854:
-            res["resolution"] = "480p"
-        case 640:
-            res["resolution"] = "360p"
-        case 426:
-            res["resolution"] = "240p"
-        case default:
-            res["resolution"] = str(width)
-
-    res["video_codec"] = video_streams[0].get("codec_name", "???").lower()
-    res["profile"] = video_streams[0].get("profile", "???").lower()
     res["bitrate"] = int(video_streams[0].get("bit_rate", "0"))
     if res["bitrate"] == 0:
         res["bitrate"] = int(media_info.get("format").get("bit_rate","0"))
     res["bitrate"]=int(res["bitrate"]/1000)
 
-    if res["video_codec"] == "h264" and res["resolution"] == "1080p" and res["bitrate"] > 20000:
-        res["toencode"] = "Yes"
+    # if res["video_codec"] == "h264" and res["resolution"] == "1080p" and res["bitrate"] > 20000:
+    #     res["toencode"] = "Yes"
 
     ## Traitement audio
-    audio_streams = [s for s in media_info.get("streams", []) if s.get("codec_type") == "audio"]
-    frenchfound = False
-    otherfound = False
-    for audio_stream in audio_streams:
-        tags = audio_stream.get("tags", {})
-        language = tags.get("language", "???")
-        languagetitle = tags.get("title", "???")
-        res["audio_codec"] += f"{language}:{audio_stream.get("codec_name", "???")} "
-        if language == "fre":
-            frenchfound = True
-        else:
-            otherfound = True
-    if frenchfound and otherfound:
-            res["toclean"] = "Yes"
-
-    ## traitement sous-titre
-    sub_streams = [s for s in media_info.get("streams", []) if s.get("codec_type") == "subtitle"]
-    for sub_stream in sub_streams:
-        language = sub_stream.get("tags", {}).get("language", "???")
-        res["sub"] += f"{language} "
-        # if language != "fre":
-        #     res["toclean"] = "Yes"
+    # audio_streams = [s for s in media_info.get("streams", []) if s.get("codec_type") == "audio"]
+    # frenchfound = False
+    # otherfound = False
+    # for audio_stream in audio_streams:
+    #     if language == "fre":
+    #         frenchfound = True
+    #     else:
+    #         otherfound = True
+    # if frenchfound and otherfound:
+    #         res["toclean"] = "Yes"
 
     ## conclusion
     if filename.find("[3D]") != -1:
-        res["statut"] = "Ignore"
         res["comment"] = "Film 3D"
-    elif res["toencode"] == "Yes":
-        res["statut"] = "ToEncode"
-    elif res["toclean"] == "Yes":
-        res["statut"] = "ToClean"
-    elif res["video_codec"] == "hevc" and res["resolution"] == "1080p":
-        res["statut"]="Good"
-    else:
-        res["statut"]=""
+    # elif res["toencode"] == "Yes":
+    #     res["statut"] = "ToEncode"
+    # elif res["toclean"] == "Yes":
+    #     res["statut"] = "ToClean"
+    # elif res["video_codec"] == "hevc" and res["resolution"] == "1080p":
+    #     res["statut"]="Good"
 
     ## Génération du ticket
     # if res["statut"] == "ToEncode" or res["statut"] == "ToClean":
-    if res["statut"] == "ToClean" and res['resolution'] == "1080p":
-        with open(ticketpath+"/"+filename+".txt", "w") as f:
-            f.write(f"action=\"{res["statut"]}\"\nfilename=\"{filename}\"\nfilepath=\"{filepath}\"\nvideo_codec=\"{res['video_codec']}\"\nresolution=\"{res['resolution']}\"\nbitrate=\"{res['bitrate']}\"\nprofile=\"{res['profile']}\"\n")
+    # if res["statut"] == "ToClean" and res['resolution'] == "1080p":
+    #     with open(ticketpath+"/"+filename+".txt", "w") as f:
+    #         f.write(f"action=\"{res["statut"]}\"\nfilename=\"{filename}\"\nfilepath=\"{filepath}\"\nvideo_codec=\"{res['video_codec']}\"\nresolution=\"{res['resolution']}\"\nbitrate=\"{res['bitrate']}\"\nprofile=\"{res['profile']}\"\n")
 
     return res
 
@@ -151,8 +201,8 @@ for filename in os.listdir(moviespath) :
         res = analyse_media(filename, fullfilename)
         filmlist.append(res)
 
-with open("check.csv", "w") as f:
-    if len(filmlist) > 0:
-        f.write(separator.join(filmlist[0].keys())+"\n")
-        for film in filmlist:
-            f.write(separator.join(str(val) for val in film.values())+"\n")
+# print(json.dumps(filmlist, indent=2, ensure_ascii=False))
+with open("check.json", 'w') as file:
+    json.dump(filmlist, file, indent=2, ensure_ascii=False)
+
+export_to_csv(filmlist, "checktemp.csv")
