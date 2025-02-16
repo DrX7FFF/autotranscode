8e933bb3157f1c1e9d53464a29ab82b40ae942da
DrX7FFF <dubourg.v@gmail.com>
2025-02-16 18:41:37 +0100

    save

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Scan file
 Select uninteresting video stream  
 Remove unused  
 
+## Installation
+```
+sh -c "$(curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/DrX7FFF/autotranscode/main/install.sh)"
+```
+
+
 # Download
 ```
 mkdir -p /storage/.kodi/userdata/tools && \
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+VERSION="1"
+
+appname="autotranscode"
+
+tempfile="/tmp/myinstall.zip"
+tempfolder="/tmp/myinstall"
+destfolder="/storage/.kodi/tools/autotranscode"
+
+echo "Installing $appname (setup version $VERSION)"
+
+# Create tools folder
+mkdir -p "$destfolder"
+
+# Download and copy App
+curl -L "https://github.com/DrX7FFF/$appname/archive/refs/heads/main.zip" -o "$tempfile"
+unzip -o "$tempfile" -d "$tempfolder"
+cp -rf "$tempfolder/$appname-main/." "$destfolder"
+rm "$tempfile"
+rm -r "$tempfolder"
+
+# Make executable
+chmod +x "$destfolder/*.py"
